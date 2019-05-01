0e1a71bccb1295b33c7a38cd98f697100f959d72
Bradley Gallardo <bradleygallardo@gmail.com>
2019-04-30 20:43:53 -0700

    Add files via upload

---
+++ Scripts/MoveLR.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLR : MonoBehaviour
+{
+	int count = 0;
+	bool up = true;
+	// Update is called once per frame
+	void Update()
+	{
+
+		if (up)
+		{
+			transform.Translate(Vector3.left * Time.deltaTime, Space.World);
+			print(count);
+			if (count < 200) ++count;
+			else up = false;
+
+		}
+		else if (!up)
+		{
+			transform.Translate(Vector3.right * Time.deltaTime, Space.World);
+			print(count);
+			if (count > 0) --count;
+			else up = true;
+		}
+	}
+}
---
+++ Scripts/MoveUD.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUD : MonoBehaviour
+{
+	int count = 0;
+	bool up = true;
+	
+	// Update is called once per frame
+	void Update()
+	{
+
+		if (up)
+		{
+			transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+			print(count);
+			if (count < 200) ++count;
+			else up = false;
+
+		}
+		else if (!up)
+		{
+			transform.Translate(Vector3.down * Time.deltaTime, Space.World);
+			print(count);
+			if (count > 0) --count;
+			else up = true;
+		}
+	}
+}
